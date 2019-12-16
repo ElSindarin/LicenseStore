9d9054b55f884ed6e545e12b3c6da8d074c91f10
Serhii Navrotskyi <sindarin.math@gmail.com>
2019-12-16 23:11:22 +0200

    Основная версия

--- src/main/java/LicenseStore.java
+++ src/main/java/LicenseStore.java
@@ -1,5 +1,6 @@
 public class LicenseStore {
     public static void main(String[] args) {
         System.out.println("Добро пожаловать в магазин лицензий!");
+        System.out.println("Это основная версия");
     }
 }
