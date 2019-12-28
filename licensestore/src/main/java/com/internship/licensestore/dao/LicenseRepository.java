0918e2b3b08ce62f32e596f813393d84bfa37965
Serhii Navrotskyi <sindarin.math@gmail.com>
2019-12-28 12:43:14 +0200

    deleted unnecessary files

--- licensestore/src/main/java/com/internship/licensestore/dao/LicenseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.internship.licensestore.dao;
-
-import com.internship.licensestore.model.LicenseEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LicenseRepository extends JpaRepository<LicenseEntity, Integer> {
-}
